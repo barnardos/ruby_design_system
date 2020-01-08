70defb34d36e9bf6ea142e4f804f276425bfe290
Marcin Jedras <mjjedras@gmail.com>
2020-01-08 16:09:39 +0000

    Attempt to fix generator test

--- spec/generators/barnardos/install_generator_spec.rb
+++ spec/generators/barnardos/install_generator_spec.rb
@@ -2,40 +2,39 @@ require 'spec_helper'
 require 'generator_spec'
 require 'generators/barnardos/install_generator'
 
+require 'tmpdir'
+
+TMP_ROOT = Pathname.new('../../../tmp')
+
+
 RSpec.describe Barnardos::Generators::InstallGenerator, type: :generator do
-  destination File.expand_path("../../../tmp", File.dirname(__FILE__))
-  teardown :cleanup_destination_root
+  # destination File.expand_path('../../../tmp', File.dirname(__FILE__))
+
+  include GeneratorSpec::TestCase
+  destination TMP_ROOT
+  # setup :prepare_destination
+  # teardown :cleanup_destination_root
   # arguments %w(something)
 
   before(:all) do
+    FileUtils.rm_rf(TMP_ROOT)
     prepare_destination
     run_generator
   end
 
-  # specify do
-  #   expect(destination_root).to have_structure {
-  #     directory '.' do
-  #       file 'postcss.config.js' do
-  #         contains 'require("postcss-reporter")'
-  #       end
-  #       file 'package.json' do
-  #         contains '@barnardos/components'
-  #       end
-  #     end
-  #   }
-  # end
-  it 'creates a test initializer' do
-    assert_file 'postcss.config.js', 'require("postcss-reporter")'
+  specify do
+    expect(destination_root).to have_structure {
+      directory '.' do
+        file 'postcss.config.js' do
+          contains 'require("postcss-reporter")'
+        end
+        file 'package.json' do
+          contains '@barnardos/components'
+        end
+      end
+    }
   end
+  # it 'creates a test initializer' do
+  #   assert_file 'postcss.config.js', 'require("postcss-reporter")'
+  # end
 end
-
-# class InstallGeneratorSpec < Rails::Generators::TestCase
-#   tests Barnardos::Generators::InstallGenerator
-#   destination File.expand_path("../../../tmp", File.dirname(__FILE__))
-#   teardown :cleanup_destination_root
-
-#   test "database.yml is not created when skipping Active Record" do
-#     run_generator
-#     assert_no_file "config/database.yml"
-#   end
-# end
